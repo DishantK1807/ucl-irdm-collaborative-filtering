6a4a967ae6c91ba495479a2b32a166a9ca163bd3
archienorman <archienorman@me.com>
2016-04-15 14:15:45 +0100

    ctr python

---
+++ invoke-ctr.py
@@ -0,0 +1,8 @@
+from subprocess import call
+
+
+########################################################################################################################
+
+#We need to compile the C program first
+
+call(["./main", "args", "to", "main"])
